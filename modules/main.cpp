02ecdad801e0f4313dfc46dd20673f868ebbc6b4
iamsuslik <dariaelts@gmail.com>
2026-04-27 17:06:52 +0300

    ...

--- modules/main.cpp
+++ modules/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include "modules/shared/include/common.h"
 #include "modules/core/include/HierarchyManager.h"
 #include "modules/logic/include/TableManager.h"
+#include "modules/api/include/SQLParser.h"
 
 int main(int argc, char* argv[]) {
     HierarchyManager hm;
+    SQLParser parser;
 
     hm.createDatabase("university_db");
     hm.useDatabase("university_db");
@@ -17,16 +20,15 @@ int main(int argc, char* argv[]) {
     };
     TableSchema studentsTable("students", cols);
 
-    std::string fullPath;
-    hm.prepareTablePath(studentsTable.table_name, fullPath);
+    Result pathRes = hm.resolveTablePath(studentsTable.table_name);
+    std::string fullPath = pathRes.path;
 
     std::cout << "--- Executing Step 3 & 4: Table and Insert Test ---\n";
 
     Result createRes = TableManager::createTable(fullPath, studentsTable);
     std::cout << "[TableManager] Create: " << createRes.message << "\n";
 
-    if (createRes.success) {
-
+    if (createRes.success || createRes.message.find("EXIST") != std::string::npos) {
         Row student1;
         student1.push_back(Value(1));      
         student1.push_back(Value("Ivan")); 
@@ -34,12 +36,27 @@ int main(int argc, char* argv[]) {
 
         Result insRes = TableManager::insertRow(fullPath, student1);
         std::cout << "[TableManager] Insert Row: " << insRes.message << "\n";
+
+        TableManager::executeSelect(fullPath, Condition(), {}, {});
     }
-    
     std::cout << "-------------------------------------------\n\n";
 
     if (argc == 1) {
-        std::cout << "Starting Simple DBMS in INTERACTIVE mode..." << std::endl;
+        std::cout << "DBMS Shell started. Type commands; end with ';'. Type 'exit' to quit.\n";
+        std::string line, buffer;
+
+        while (true) {
+            std::cout << (buffer.empty() ? "sql> " : "  -> ");
+            if (!std::getline(std::cin, line)) break;
+            if (line == "exit") break;
+
+            buffer += line + " ";
+            size_t pos;
+            while ((pos = buffer.find(';')) != std::string::npos) {
+                parser.process(buffer.substr(0, pos), hm);
+                buffer.erase(0, pos + 1);
+            }
+        }
     } 
     else if (argc == 2) {
         std::cout << "Starting Simple DBMS in BATCH mode. Script: " << argv[1] << std::endl;
